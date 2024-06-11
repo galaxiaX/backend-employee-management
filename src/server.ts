a31f42ae594e6d702bee83ca16ddd9ee08299d63
Namo <pongsathon149@gmail.com>
2024-06-11 12:57:18 +0700

    chore: Update CORS origin to use environment variable or fallback to localhost

--- src/server.ts
+++ src/server.ts
@@ -26,8 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.MAIN_URL || "*",
-    // origin: process.env.MAIN_URL || "http://localhost:3000",
+    origin: process.env.MAIN_URL || "http://localhost:3000",
     credentials: true,
   })
 );
